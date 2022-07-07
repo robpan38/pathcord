5f12d317ade713a4eeab8f009aef78a7a39788c1
Robert Pandele <robert.pandele@uipath.com>
2022-07-07 11:38:44 +0300

    initial commit v2

---
+++ server.js
@@ -0,0 +1,15 @@
+// import express from 'express';
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/login.html");
+})
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
