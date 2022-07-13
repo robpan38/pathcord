c89ad1380ff3f0de20913c521f7dd54765b529db
Robert Pandele <robert.pandele@uipath.com>
2022-07-13 13:50:18 +0300

    (fix): refactor login logic from js to ts

--- scripts/loginLogic.ts
+++ scripts/loginLogic.ts
@@ -1,19 +1,20 @@
-(function () {
-    var loginInput = document.querySelector(".login-input-field");
-    var loginBtn = document.querySelector(".login-btn");
-    loginInput.addEventListener("input", function () {
+(function() {
+    const loginInput = document.querySelector<HTMLInputElement>(".login-input-field")!;
+    const loginBtn = document.querySelector<HTMLButtonElement>(".login-btn")!;
+
+    loginInput.addEventListener("input", () => {
         if (loginInput.value === "" && loginBtn.classList.contains("active-login-btn")) {
             loginBtn.classList.remove("active-login-btn");
-        }
-        else if (loginInput.value !== "" && !loginBtn.classList.contains("active-login-btn")) {
+        } else if (loginInput.value !== "" && !loginBtn.classList.contains("active-login-btn")) {
             loginBtn.classList.add("active-login-btn");
         }
     });
-    loginBtn.addEventListener("click", function () {
+
+    loginBtn.addEventListener("click", () => {
         if (loginBtn.classList.contains("active-login-btn")) {
             loginBtn.classList.remove("active-login-btn");
             loginInput.value = "";
             location.href = "./chat.html";
         }
-    });
+    })
 })();
