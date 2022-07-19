801fd02d5af7c65ec156ad5c832016c431631726
mihneat <mihnea.tudor01@gmail.com>
2022-07-19 18:52:26 +0300

    feat: reworked login logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginContainerComponent } from './login/login-container/login-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginContainerComponent },
+  { path: 'chat', component: LoginContainerComponent },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginInputContainerComponent } from './login/login-input-container/login-input-container.component';
+import { LogoContainerComponent } from './login/logo-container/logo-container.component';
+import { LoginContainerComponent } from './login/login-container/login-container.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginInputContainerComponent,
+    LogoContainerComponent,
+    LoginContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
