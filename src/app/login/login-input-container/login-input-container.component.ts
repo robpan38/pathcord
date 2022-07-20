af9671a1f7b7ecedc3543a0ea26858fbad4afcbf
Robert Pandele <robert.pandele@uipath.com>
2022-07-20 15:33:48 +0300

    fix: fixed the css issues caused by the fact that angular modified the initial structure of the document

--- src/app/login/login-input-container/login-input-container.component.ts
+++ src/app/login/login-input-container/login-input-container.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
 import { Router } from '@angular/router';
 
 const chatRoute: string = '/chat'
