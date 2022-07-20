d325c26d5cced2212a5ce33808a4c502074fc1ec
mihneat <mihnea.tudor01@gmail.com>
2022-07-20 17:26:31 +0300

    feat: created angular structure for the chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 import { LoginContainerComponent } from './login/login-container/login-container.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginContainerComponent },
-  { path: 'chat', component: LoginContainerComponent },
-  { path: '**', redirectTo: '/home' }
+  { path: 'chat', component: ChatComponent },
+  { path: '**', redirectTo: '/login' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,23 @@ import { LoginInputContainerComponent } from './login/login-input-container/logi
 import { LogoContainerComponent } from './login/logo-container/logo-container.component';
 import { LoginContainerComponent } from './login/login-container/login-container.component';
 import { FormsModule } from '@angular/forms';
+import { ChatComponent } from './chat/chat.component';
+import { AppContainerComponent } from './chat/app-container/app-container.component';
+import { HeaderContainerComponent } from './chat/header-container/header-container.component';
+import { ChatContainerComponent } from './chat/app-container/chat-container/chat-container.component';
+import { UsersContainerComponent } from './chat/app-container/users-container/users-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginInputContainerComponent,
     LogoContainerComponent,
-    LoginContainerComponent
+    LoginContainerComponent,
+    ChatComponent,
+    AppContainerComponent,
+    HeaderContainerComponent,
+    ChatContainerComponent,
+    UsersContainerComponent
   ],
   imports: [
     BrowserModule,
